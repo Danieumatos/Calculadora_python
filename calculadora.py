819541dd82175847d6efa50e65cd8a264914c9f8
Danieumatos <dani_matos00@hotmail.com>
2024-09-17 11:23:10 -0300

    Alteração das funcionalidades. Criação de funções para executar as operações e a calculadora.

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,8 @@
-import os;
+import os
 
-opcao = 0;
-while opcao != 6:
-    os.system('cls')
+#Função que imprime o menu
+def menu():
+    limpar_tela()
     print("""Calculadora
           [1] Soma
           [2] Subtração
@@ -11,23 +11,58 @@ while opcao != 6:
           [5] Exponenciação
           [6] Encerrar o programa
           """);
-    
-    opcao = int(input("Digite a opção desejada: "));
-    
-    while opcao == 1:
-        os.system('cls');
-        print("Soma");
-        num1 = int(input("Digite o primeiro número: "));
-        num2 = int(input("Digite o segundo número: "));
-        print("O resultado da soma é {}".format(num1 + num2));
-        
-        opcao = input("Deseja realizar outra operação?  Digite 1 para sim e 0 para não");
 
-    
-    if opcao == 2:
-        os.system('cls');
-        print("Subtração");
-        num1 = int(input("Digite o primeiro número: "));
-        num2 = int(input("Digite o segundo número: "));
-        print("O resultado da soma é {}".format(num1 - num2));
-        continue
+def soma(num1, num2):
+    return num1 + num2
+
+def subtracao(num1, num2):
+    return num1 - num2
+
+def multiplicacao(num1, num2):
+    return num1 * num2
+
+def divisao(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        print("Erro! Divisão por 0")
+
+def exponenciacao(num1, num2):
+    return num1 ** num2
+
+def limpar_tela():
+    os.system('cls')
+
+def calculadora():
+    limpar_tela()
+    opcao = 0
+    while opcao != 6:
+        menu()
+        
+        opcao = int(input("Digite a opção desejada: "))
+        
+        
+        if opcao in [1, 2, 3, 4, 5]:
+            limpar_tela()
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+        
+            if opcao == 1:
+                print(f'Resultado: {soma(num1, num2)}')
+            elif opcao == 2:
+                print(f'Resultado: {subtracao(num1, num2)}')
+            elif opcao == 3:
+                print(f'Resultado: {multiplicacao(num1, num2)}')
+            elif opcao == 4:
+                print(f'Resultado: {divisao(num1, num2)}')
+            elif opcao == 5:
+                print(f'Resultado: {exponenciacao(num1, num2)}')
+            input("Pressione enter para continuar...")
+        
+        elif opcao == 6:
+            print("Encerrando o programa...")
+        else:
+            print("Opção inválida! Tente novamente.")
+            input("Pressione enter para continuar...")
+            
+calculadora()
